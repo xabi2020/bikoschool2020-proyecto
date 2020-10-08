8a9afdf3f4c6c57882b1116e346abeb3a711c79f
Carlos Martinez <carlos.martinez@biko2.com>
2020-10-08 16:40:45 +0200

    Adds comments to middlewares

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,10 +4,19 @@ import logger from 'morgan'
 
 const app = express()
 
+// Shows request log on terminal
+// https://github.com/expressjs/morgan
 app.use(logger('combined'))
+
+// Parses incoming requests with JSON payloads
+// http://expressjs.com/es/api.html#express.json
 app.use(express.json())
+
+// Parses incoming requests with urlencoded payloads
+// http://expressjs.com/es/api.html#express.urlencoded
 app.use(express.urlencoded({ extended: false }))
 
+// Routes every path
 app.use('/', (req: Request, res: Response) => {
   res.json({data: "index!"})
 })
