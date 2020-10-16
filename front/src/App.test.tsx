a3b471fb0f9f180bd9cb6cbcf9082783b813c5fe
sanchezandoni <andoni@berritu.com>
2020-10-16 20:21:13 +0200

    feat: muestra un listado de memes

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App'
+import Memes from './memes.json'
 
 test('muestra el meme Movie Brazil', () => {
   render(<App />)
 
-  expect(screen.getByText(/Movie Brazil GIF by MOODMAN/i)).toBeInTheDocument()
+  expect(screen.getByText('Movie Brazil GIF by MOODMAN')).toBeInTheDocument()
+})
+
+test('muestra un listado de memes', () => {
+  render(<App />)
+
+  for (let meme of Memes) {
+    expect(screen.getByText(meme.title)).toBeInTheDocument()
+  }
 })
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import Memes from './memes.json'
 
 const App: React.FC = () => {
-  return <>Movie Brazil GIF by MOODMAN!</>
+  return (
+    <section>
+      {Memes.map((meme) => (
+        <div key={meme.id}>
+          <p>{meme.title}</p>
+          <img title={meme.title} />
+        </div>
+      ))}
+    </section>
+  )
 }
 
 export default App
