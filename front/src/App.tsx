9e385f21ebbf3348d912b1add13c510c46bf70db
Marcos Galvez <galvez.115053@e.unavarra.es>
2020-10-16 20:27:03 +0200

    Pasado primer test a verde

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <>
-      <p>"Movie Brazil GIF by MOODMAN"</p>
+      <img src="https://media4.giphy.com/media/YleuWir5NTNVXkflSp/giphy.gif?cid=be655fb7f245f7d29df0fc743b70e3ee884dbaf31956e789&rid=giphy.gif" alt="Movie Brazil GIF by MOODMAN"></img>
     </>
   );
 }
