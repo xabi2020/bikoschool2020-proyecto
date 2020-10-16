219445b11df73f83f62201ae239e889e22ced98d
miriam-gaviria2 <52879306+miriam-gaviria2@users.noreply.github.com>
2020-10-16 20:34:17 +0200

    fix: error app types

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { memes } from './fixture/recent.json'
 
-function App() {
-  return memes.map((meme) => (
-    <img alt={meme.title} key={meme.id} src={meme.images.small.url} />
-  ))
-}
+const App: React.FC = () => (
+  <>
+    {memes.map((meme) => (
+      <img alt={meme.title} key={meme.id} src={meme.images.small.url} />
+    ))}
+  </>
+)
+
 export default App
