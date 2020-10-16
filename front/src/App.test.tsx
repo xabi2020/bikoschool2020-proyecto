afaac683f6d6ed3106d5baf1ac5de72a995cdce4
Miguel Echávarri <miguelepmrs@gmail.com>
2020-10-16 20:05:00 +0200

    test listado de memes en rojo

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App'
+import memes from './fixtures/memes.json'
 
 describe('List of memes', () => {
   it('has a list of memes including "Movie Brazil GIF by MOODMAN"', async () => {
     render(<App />)
 
-    const boulbasourTextElement = await screen.findByText(
+    const movieBrazilTextElement = await screen.findByText(
       /Movie Brazil GIF by MOODMAN/i,
     )
 
-    expect(boulbasourTextElement).toBeInTheDocument()
+    expect(movieBrazilTextElement).toBeInTheDocument()
+  })
+
+  it('has a list of memes', async () => {
+    render(<App />)
+
+    for (let meme of memes) {
+      const memeTextElement = await screen.findByText(meme.title)
+      expect(memeTextElement).toBeInTheDocument()
+    }
   })
 })
