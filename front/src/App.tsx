05a86843d014383ec4c1694078b0ebd709a1cff6
miriam-gaviria2 <52879306+miriam-gaviria2@users.noreply.github.com>
2020-10-16 19:54:56 +0200

    feat: show memes according src attribute

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,7 +3,7 @@ import { memes } from './fixture/recent.json'
 
 function App() {
   return memes.map((meme) => (
-    <img alt={meme.title} key={meme.id} src="image.gif" />
+    <img alt={meme.title} key={meme.id} src={meme.images.small.url} />
   ))
 }
 export default App
