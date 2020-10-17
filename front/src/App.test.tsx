0012ed8c6d62937fd214a1b44dfd856a53ee7dfd
Miguel Echávarri <miguelepmrs@gmail.com>
2020-10-17 14:06:10 +0200

    Test en rojo: gestión de errores

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App'
 import memes from './fixtures/memes.json'
+import { server } from './mocks/server'
+import { rest } from 'msw'
+
+const errorMessage = 'Se ha producido un error'
 
 describe('List of memes', () => {
   it('should show memes', async () => {
@@ -14,5 +18,23 @@ describe('List of memes', () => {
       ).toHaveAttribute('src', meme.image.url)
       expect(memeTextElement).toBeInTheDocument()
     }
+  })
+
+  it('should show memes', async () => {
+    server.use(
+      rest.get('http://localhost:3001/api/v1/memes', (req, res, ctx) =>
+        res(
+          ctx.status(500),
+          ctx.json({
+            errorMessage,
+          }),
+        ),
+      ),
+    )
+
+    render(<App />)
+
+    const errorElement = await screen.findByText(errorMessage)
+    expect(errorElement).toBeInTheDocument()
   })
 })
