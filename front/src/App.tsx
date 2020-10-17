4189a0785b29aab54eeac2b41da163c45704ba64
Jesús Olazagoitia <jesus.designer@gmail.com>
2020-10-17 10:16:40 +0200

    refactor: Extract api call

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,27 @@
 import React, { useEffect, useState } from 'react'
 
-const App: React.FC = () => {
-  const [ memes, setMemes] = useState([]);  
-  useEffect(() => {
-    fetch("http://127.0.0.1/")
-      .then(res => res.json())
-      .then(({ memes }) => setMemes(memes))
-  }, [])
+interface Meme {
+  title: string
+  id: string
+  images: {
+    small: {
+      url: string
+    }
+  }
+}
 
-  return ( 
+async function getMemes(): Promise<Meme[]> {
+  const response = await fetch('http://127.0.0.1/')
+  const { memes } = await response.json()
+  return memes
+}
+
+const App: React.FC = () => {
+  const [memes, setMemes] = useState<Meme[]>([])
+  useEffect(() => {
+    getMemes().then(setMemes)
+  }, [])
+  return (
     <>
       {memes?.map((meme) => (
         <img alt={meme.title} key={meme.id} src={meme.images.small.url} />
