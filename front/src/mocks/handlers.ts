bda6ee471c4ea78e110312a22f3dae87c1d85513
Jesús Olazagoitia <jesus.designer@gmail.com>
2020-10-17 09:34:59 +0200

    chore: configure MSW

---
+++ front/src/mocks/handlers.ts
@@ -0,0 +1,11 @@
+import { rest } from 'msw'
+import recents from "../fixture/recent.json";
+
+export const handlers = [
+  rest.get('127.0.0.1/', (req, res, ctx) =>
+    res(
+      ctx.status(200),
+      ctx.json(recents)
+    )
+  )
+]
