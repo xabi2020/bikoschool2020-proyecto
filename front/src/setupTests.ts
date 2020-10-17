c71c948d3a3fe6c687a85a538c05257675aa81ce
Miguel Echávarri <miguelepmrs@gmail.com>
2020-10-17 12:32:56 +0200

    Test en verde control lista de memes

--- front/src/setupTests.ts
+++ front/src/setupTests.ts
@@ -2,8 +2,6 @@ import '@testing-library/jest-dom/extend-expect'
 
 import { server } from './mocks/server'
 
-process.env.REACT_APP_API_URL = 'http://localhost:3001/api/v1'
-
 beforeAll(() => server.listen({ onUnhandledRequest: 'warn' }))
 
 afterEach(() => server.resetHandlers())
