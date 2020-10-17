97200541f99d8996e3a01878d784e40c4996e074
miriam-gaviria2 <52879306+miriam-gaviria2@users.noreply.github.com>
2020-10-17 13:16:32 +0200

    test: Should show the results when user searchs by movie tag

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { server } from './mocks/server'
 import { rest } from 'msw'
 import App from './App'
 import { memes } from './fixture/recent.json'
+import searchResultMovie from './fixture/search_movie.json'
 
 describe('Listado de memes', () => {
   it('should recibe meme list from API', async () => {
@@ -30,5 +31,20 @@ describe('Search memes', () => {
     render(<App />)
 
     expect(screen.getByRole('searchbox')).toBeInTheDocument()
+  })
+  it('Should show the results from user search', async () => {
+    jest.spyOn(window, 'fetch')
+
+    render(<App />)
+    const inputSearch = screen.getByRole('searchbox')
+    fireEvent.change(inputSearch, { target: { value: 'movie' } })
+
+    for (const meme of searchResultMovie.memes) {
+      const image = await screen.findByRole('img', { name: meme.title })
+      expect(image).toBeInTheDocument()
+      expect(image).toHaveAttribute('src', meme.images.small.url)
+    }
+
+    expect(window.fetch).toBeCalledWith('http://127.0.0.1/search?movie')
   })
 })
