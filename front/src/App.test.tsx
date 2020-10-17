e8a3685f1661fbeb5e1dd5d6a85c5074e4fcb9c3
calvo111979 <acalvoinfa10@gmail.com>
2020-10-17 09:44:10 +0200

    show a meme

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -3,15 +3,13 @@ import { render, screen } from '@testing-library/react'
 import App from './App'
 
 describe('Listado de memes', () => {
-  it('should be a title in the document', () => {
+  it('Should show a meme', () => {
     render(<App />)
-    const titulo = screen.getByText('Listado ', { exact: false })
-    expect(titulo).toBeInTheDocument()
-  })
-
-  it('should have several images',() => {
-    render(<App />)
-    const listadoImagenes = screen.getAllByRole('img')
-    expect(listadoImagenes.length).toBeGreaterThan(1)
+    const meme = screen.getByRole('img', { name: 'Meme de shrek' })
+    expect(meme).toBeInTheDocument()
+    expect(meme).toHaveAttribute(
+      'src',
+      'https://i.pinimg.com/originals/52/8M6/fe/5286fe86fbdd8ec13c4e81bde0e3dfdf.jpg',
+    )
   })
 })
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -6,8 +6,7 @@ function App() {
   return (
     <>
     <div>Listado de memes</div>
-    <img src='https://i.pinimg.com/originals/52/86/fe/5286fe86fbdd8ec13c4e81bde0e3dfdf.jpg' alt='Imagen de prueba'/>
-    <img src='https://i.pinimg.com/originals/52/86/fe/5286fe86fbdd8ec13c4e81bde0e3dfdf.jpg' alt='Imagen de prueba'/>
+    <img src='https://i.pinimg.com/originals/52/8M6/fe/5286fe86fbdd8ec13c4e81bde0e3dfdf.jpg' alt='Meme de shrek'/>
     </>
   );
 }
