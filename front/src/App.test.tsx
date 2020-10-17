7da2f01b09121276feea4d2069e63e1bbc300166
sanchezandoni <andoni@berritu.com>
2020-10-17 09:42:15 +0200

    fix: muestra un listado de memes (BIEN)

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -17,4 +17,16 @@ describe('listado de memes', () => {
       'https://media4.giphy.com/media/YleuWir5NTNVXkflSp/giphy.gif?cid=be655fb7f245f7d29df0fc743b70e3ee884dbaf31956e789&rid=giphy.gif',
     )
   })
+
+  test('muestra un listado de memes', () => {
+    render(<App />)
+
+    for (let memeItem of Memes) {
+      const meme = screen.getByRole('img', {
+        name: memeItem.title,
+      })
+
+      expect(meme).toHaveAttribute('src', memeItem.src)
+    }
+  })
 })
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import Memes from './memes.json'
 
 const App: React.FC = () => {
   return (
-    <img
-      title="Movie Brazil GIF by MOODMAN"
-      src="https://media4.giphy.com/media/YleuWir5NTNVXkflSp/giphy.gif?cid=be655fb7f245f7d29df0fc743b70e3ee884dbaf31956e789&rid=giphy.gif"
-    />
+    <>
+      {Memes.map((meme) => (
+        <img alt={meme.title} src={meme.src} key={meme.id} />
+      ))}
+    </>
   )
 }
-
 export default App
