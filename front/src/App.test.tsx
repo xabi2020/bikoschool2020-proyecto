9fcb4dc22fea0485d75eba48cf48af756b898ff8
miriam-gaviria2 <52879306+miriam-gaviria2@users.noreply.github.com>
2020-10-17 10:43:21 +0200

    refactor: delete unnecesary test

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -4,19 +4,8 @@ import App from './App'
 import { memes } from './fixture/recent.json'
 
 describe('Listado de memes', () => {
-  it('should show meme with title Movie Brazil GIF', async() => {
+  it('should recibe meme list from API', async () => {
     render(<App />)
-    expect(
-      await screen.findByAltText('Movie Brazil GIF by MOODMAN'),
-    ).toBeInTheDocument()
-  })
- 
-  it('should recibe meme list form API', async () => {
-    jest.spyOn(window, 'fetch')
-    
-    render(<App />)
-
-    expect(window.fetch).toBeCalledWith("http://127.0.0.1/")
 
     for (const meme of memes) {
       const image = await screen.findByRole('img', { name: meme.title })
