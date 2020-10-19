bf76620901330b34826ec080c82ed2c25653e335
Carlos Martinez <carlos.martinez@biko2.com>
2020-10-19 18:21:14 +0200

    Aplica la configuración de prettier

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,17 +1,17 @@
-import express, { Request, Response } from 'express';
-import logger from 'morgan';
+import express, { Request, Response } from 'express'
+import logger from 'morgan'
 
-export const app = express();
+export const app = express()
 // Shows request log on terminal
 // https://github.com/expressjs/morgan
-app.use(logger('combined'));
+app.use(logger('combined'))
 // Parses incoming requests with JSON payloads
 // http://expressjs.com/es/api.html#express.json
-app.use(express.json());
+app.use(express.json())
 // Parses incoming requests with urlencoded payloads
 // http://expressjs.com/es/api.html#express.urlencoded
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }))
 // Routes every path
 app.use('/', (req: Request, res: Response) => {
-  res.json({ data: "index!" });
-});
+  res.json({ data: 'index!' })
+})
