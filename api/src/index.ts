c7c2d43b0b7dda6c879cb5543f3e9577a9a1ab32
Carlos Martinez <carlos.martinez@biko2.com>
2020-10-19 18:12:18 +0200

    Extrae app

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,25 +1,5 @@
 import http from 'http'
-import express, { Request, Response } from 'express'
-import logger from 'morgan'
-
-const app = express()
-
-// Shows request log on terminal
-// https://github.com/expressjs/morgan
-app.use(logger('combined'))
-
-// Parses incoming requests with JSON payloads
-// http://expressjs.com/es/api.html#express.json
-app.use(express.json())
-
-// Parses incoming requests with urlencoded payloads
-// http://expressjs.com/es/api.html#express.urlencoded
-app.use(express.urlencoded({ extended: false }))
-
-// Routes every path
-app.use('/', (req: Request, res: Response) => {
-  res.json({data: "index!"})
-})
+import { app } from './app'
 
 var port = process.env.PORT || '3000'
 app.set('port', port)
